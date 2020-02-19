6acc8ced9bdb6f2c90b521cadb482664f4f9f26c
cyct123 <cyct123@gmail.com>
2020-02-19 23:54:21 +0800

    Finish Python Version of LeetCode#70

--- 70.climbing-stairs.py
+++ 70.climbing-stairs.py
@@ -48,7 +48,10 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         if n <= 1:
             return 1
-        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
-
+        f0 = f1 = 1
+        for i in range(2, n + 1):
+            f = f0 + f1
+            f0, f1 = f1, f
+        return f
 # @lc code=end
 
