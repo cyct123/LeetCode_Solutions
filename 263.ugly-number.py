75d9da1e90fcf6f97ad2b8e93872530fe61afcb9
cyct123 <cyct123@gmail.com>
2020-03-02 00:04:19 +0800

    Optimaze Python Soltion of LeetCode#263

--- 263.ugly-number.py
+++ 263.ugly-number.py
@@ -53,16 +53,14 @@ class Solution:
     def isUgly(self, num: int) -> bool:
         if num <= 0:
             return False
-        else:
-            if num <= 2:
-                return True
-            else:
-                while num % 2 == 0:
-                    num /= 2
-                while num % 3 == 0:
-                    num /= 3
-                while num % 5 == 0:
-                    num /= 5
-                return num == 1
+        if num <= 2:
+            return True
+        while num % 2 == 0:
+            num //= 2
+        while num % 3 == 0:
+            num //= 3
+        while num % 5 == 0:
+            num //= 5
+        return num == 1
 # @lc code=end
 
