344012d03fea1f37a19422db97888a4fadccd467
cyct123 <cyct123@gmail.com>
2020-03-03 19:03:07 +0800

    Finish Python Version of LeetCode#113

---
+++ 113.path-sum-ii.py
@@ -0,0 +1,58 @@
+#
+# @lc app=leetcode id=113 lang=python3
+#
+# [113] Path Sum II
+#
+# https://leetcode.com/problems/path-sum-ii/description/
+#
+# algorithms
+# Medium (44.07%)
+# Likes:    1361
+# Dislikes: 44
+# Total Accepted:    294.5K
+# Total Submissions: 663.8K
+# Testcase Example:  '[5,4,8,11,null,13,4,7,2,null,null,5,1]\n22'
+#
+# Given a binary tree and a sum, find all root-to-leaf paths where each path's
+# sum equals the given sum.
+# 
+# Note: A leaf is a node with no children.
+# 
+# Example:
+# 
+# Given the below binary tree and sum = 22,
+# 
+# 
+# ⁠     5
+# ⁠    / \
+# ⁠   4   8
+# ⁠  /   / \
+# ⁠ 11  13  4
+# ⁠/  \    / \
+# 7    2  5   1
+# 
+# 
+# Return:
+# 
+# 
+# [
+# ⁠  [5,4,11,2],
+# ⁠  [5,8,4,5]
+# ]
+# 
+# 
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def pathSum(self, root: TreeNode, sum: int) -> List[List[int]]:
+        
+# @lc code=end
+
