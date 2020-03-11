15c0f21f09663bc7c0cd32a573ae1b03e9196d63
cyct123 <cyct123@gmail.com>
2020-03-11 23:23:47 +0800

    Finish Python Version of LeetCode#113

--- 113.path-sum-ii.py
+++ 113.path-sum-ii.py
@@ -15,14 +15,14 @@
 #
 # Given a binary tree and a sum, find all root-to-leaf paths where each path's
 # sum equals the given sum.
-# 
+#
 # Note: A leaf is a node with no children.
-# 
+#
 # Example:
-# 
+#
 # Given the below binary tree and sum = 22,
-# 
-# 
+#
+#
 # ⁠     5
 # ⁠    / \
 # ⁠   4   8
@@ -30,17 +30,17 @@
 # ⁠ 11  13  4
 # ⁠/  \    / \
 # 7    2  5   1
-# 
-# 
+#
+#
 # Return:
-# 
-# 
+#
+#
 # [
 # ⁠  [5,4,11,2],
 # ⁠  [5,8,4,5]
 # ]
-# 
-# 
+#
+#
 #
 
 # @lc code=start
@@ -52,7 +52,22 @@
 #         self.right = None
 
 class Solution:
+
+    def __init__(self):
+        self.stack = []
+        self.res = []
+
     def pathSum(self, root: TreeNode, sum: int) -> List[List[int]]:
-        
+        if not root:
+            return []
+        self.stack.append(root.val)
+        sum -= root.val
+        if not sum and not root.left and not root.right:
+            self.res.append(self.stack[:])
+        self.pathSum(root.left, sum)
+        self.pathSum(root.right, sum)
+        self.stack.pop()
+        return self.res
+
 # @lc code=end
 
