838af9fa0645e3db17014b61f07b421644127d64
chengziwei <chengziwei@mininglamp.com>
2022-07-15 15:58:58 +0800

    feat: optimize 977.squares-of-a-sorted-array.cpp

--- 977.squares-of-a-sorted-array.cpp
+++ 977.squares-of-a-sorted-array.cpp
@@ -6,38 +6,37 @@
 
 // @lc code=start
 //#include <vector>
-//#include <cmath>
 //#include <iostream>
-//#include <algorithm>
-//using std::reverse;
 //using std::cout;
 //using std::endl;
-//using std::abs;
 //using std::vector;
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        vector<int> squareNums;
+        vector<int> squareNums(nums.size(), 0);
         int left = 0;
         int right = nums.size() - 1;
+        int index = nums.size() - 1;
         while (left != right) {
-            if (abs(nums[left]) >= abs(nums[right])) {
-                squareNums.push_back(nums[left] * nums[left]);
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+            if (leftSquare >= rightSquare) {
+                squareNums[index] = leftSquare;
                 ++left;
             } else {
-                squareNums.push_back(nums[right] * nums[right]);
+                squareNums[index] = rightSquare;
                 --right;
             }
+            --index;
         }
-        squareNums.push_back(nums[left] * nums[left]);
-        reverse(squareNums.begin(), squareNums.end());
+        squareNums[index] = nums[left] * nums[left];
         return squareNums;
     }
 };
 
 
 //int main() {
-//    vector<int> nums{1};
+//    vector<int> nums{-7,-3,2,3,11};
 //    Solution s = Solution();
 //    vector<int> ret = s.sortedSquares(nums);
 //    for (auto itr = ret.begin(); itr != ret.end(); ++itr)
