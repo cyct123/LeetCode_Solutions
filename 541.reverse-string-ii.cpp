33ab1a2431fa3fb298e958e8a885b382a6dfbf7a
chengziwei <chengziwei@mininglamp.com>
2022-08-01 21:25:02 +0800

    feat: finish reverse function version of 541.reverse-string-ii.cpp

--- 541.reverse-string-ii.cpp
+++ 541.reverse-string-ii.cpp
@@ -8,22 +8,17 @@
 #include <string>
 #include <algorithm>
 using std::string;
-using std::swap;
+using std::reverse;
 
 
 class Solution {
 public:
     string reverseStr(string s, int k) {
-        for (int i = 0; i < s.size() / k + 1; ++i) {
-            if (!(i % 2)) {
-                int left = i * k;
-                int right = s.size() > (i + 1) * k ? (i + 1) * k - 1: s.size() - 1;
-                while (left < right) {
-                    swap(s[left], s[right]);
-                    ++left;
-                    --right;
-                }
-            }
+        for (int i = 0; i < s.size(); i += (2 * k)) {
+            if (i + k < s.size())
+                reverse(s.begin() + i, s.begin() + i + k);
+            else
+                reverse(s.begin() + i, s.end());
         }
         return s;
     }
