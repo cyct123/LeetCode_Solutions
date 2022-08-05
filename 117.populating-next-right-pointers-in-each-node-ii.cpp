f56da9403d2fa52295a2606b530a095f0d1042d7
chengziwei <chengziwei@mininglamp.com>
2022-08-05 23:15:38 +0800

    feat: finish 117.populating-next-right-pointers-in-each-node-ii.cpp

---
+++ 117.populating-next-right-pointers-in-each-node-ii.cpp
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode id=117 lang=cpp
+ *
+ * [117] Populating Next Right Pointers in Each Node II
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+//#include <queue>
+//using std::queue;
+//#include <cstddef>
+//
+//class Node {
+//public:
+//    int val;
+//    Node* left;
+//    Node* right;
+//    Node* next;
+//
+//    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+//
+//    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+//
+//    Node(int _val, Node* _left, Node* _right, Node* _next)
+//        : val(_val), left(_left), right(_right), next(_next) {}
+//};
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        queue<Node*> q;
+        if (root) q.push(root);
+        while (!q.empty()) {
+            int size = q.size();
+            Node* prev;
+            for (int i = 0; i != size; ++i) {
+                Node* cur = q.front();
+                q.pop();
+                if (i) prev->next = cur;
+                prev = cur;
+                if (cur->left) q.push(cur->left);
+                if (cur->right) q.push(cur->right);
+            }
+        }
+        return root;
+    }
+};
+// @lc code=end
+
