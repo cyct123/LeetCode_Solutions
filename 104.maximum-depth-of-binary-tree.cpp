725f1b1942f20a868f9f0b5a44b29524f7de3464
chengziwei <chengziwei@mininglamp.com>
2022-08-05 20:14:38 +0800

    feat: finish 104.maximum-depth-of-binary-tree.cpp

---
+++ 104.maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=104 lang=cpp
+ *
+ * [104] Maximum Depth of Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+//#include <queue>
+//using std::queue;
+//
+//struct TreeNode {
+//    int val;
+//    TreeNode *left;
+//    TreeNode *right;
+//    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//};
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        queue<TreeNode*> q;
+        if (root) q.push(root);
+        int depth = 0; 
+        while (!q.empty()) {
+            int size = q.size();
+            for (int i = 0; i != size; ++i) {
+                TreeNode* cur = q.front();
+                q.pop();
+                if (cur->left) q.push(cur->left);
+                if (cur->right) q.push(cur->right);
+            }
+            ++depth;
+        }
+        return depth;
+    }
+};
+// @lc code=end
+
