d1f71a7befd023b37251e47c4d2276175b2574ea
chengziwei <chengziwei@mininglamp.com>
2022-08-05 16:14:15 +0800

    feat: optimize iterative version of 144.binary-tree-preorder-traversal.cpp

--- 144.binary-tree-preorder-traversal.cpp
+++ 144.binary-tree-preorder-traversal.cpp
@@ -16,21 +16,19 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-#include <vector>
-using std::vector;
-#include <stack>
-using std::stack;
-#include <unordered_set>
-using std::unordered_set;
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
+//#include <vector>
+//using std::vector;
+//#include <stack>
+//using std::stack;
+//
+//struct TreeNode {
+//    int val;
+//    TreeNode *left;
+//    TreeNode *right;
+//    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//};
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
@@ -38,16 +36,12 @@ public:
         if(!root) return res;
         stack<TreeNode*> s;
         s.push(root);
-        unordered_set<TreeNode*> visited;
         while (!s.empty()) {
             TreeNode* cur = s.top();
-            visited.insert(cur);
             s.pop();
             res.push_back(cur->val);
-            if(cur->right && !visited.count(cur->right))
-                s.push(cur->right);
-            if(cur->left && !visited.count(cur->left))
-                s.push(cur->left);
+            if(cur->right) s.push(cur->right);
+            if(cur->left) s.push(cur->left);
         }
         return res;
     }
