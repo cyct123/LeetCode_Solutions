8dd78ae638110dc7e33ef72af585ee7367a0977d
chengziwei <chengziwei@mininglamp.com>
2022-08-05 15:22:21 +0800

    feat: finish iterative version of 144.binary-tree-preorder-traversal.cpp

--- 144.binary-tree-preorder-traversal.cpp
+++ 144.binary-tree-preorder-traversal.cpp
@@ -16,30 +16,40 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-//#include <vector>
-//using std::vector;
-//
-//struct TreeNode {
-//    int val;
-//    TreeNode *left;
-//    TreeNode *right;
-//    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-//    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-//    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-//};
+#include <vector>
+using std::vector;
+#include <stack>
+using std::stack;
+#include <unordered_set>
+using std::unordered_set;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> res;
-        traversal(root, res);
+        if(!root) return res;
+        stack<TreeNode*> s;
+        s.push(root);
+        unordered_set<TreeNode*> visited;
+        while (!s.empty()) {
+            TreeNode* cur = s.top();
+            visited.insert(cur);
+            s.pop();
+            res.push_back(cur->val);
+            if(cur->right && !visited.count(cur->right))
+                s.push(cur->right);
+            if(cur->left && !visited.count(cur->left))
+                s.push(cur->left);
+        }
         return res;
-    }
-private:
-    void traversal(TreeNode* root, vector<int>& v) {
-        if (!root) return;
-        v.push_back(root->val);
-        traversal(root->left, v);
-        traversal(root->right, v);
     }
 };
 // @lc code=end
