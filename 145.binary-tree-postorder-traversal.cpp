9e9d1bf2fe6b7b0052ff714f84372ff00f5c15fb
chengziwei <chengziwei@mininglamp.com>
2022-08-05 15:44:38 +0800

    feat: finish recursive version of 145.binary-tree-postorder-traversal.cpp

--- 145.binary-tree-postorder-traversal.cpp
+++ 145.binary-tree-postorder-traversal.cpp
@@ -16,45 +16,31 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-//#include <vector>
-//using std::vector;
-//#include <stack>
-//using std::stack;
-//#include <unordered_set>
-//using std::unordered_set;
-//
-//struct TreeNode {
-//    int val;
-//    TreeNode *left;
-//    TreeNode *right;
-//    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-//    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-//    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-//};
+#include <vector>
+using std::vector;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> res;
-        if (!root) return res;
-        stack<TreeNode*> s;
-        s.push(root);
-        unordered_set<TreeNode*> visited;
-        while (!s.empty()) {
-            TreeNode* cur = s.top();
-            if(cur->left && !visited.count(cur->left)) {
-                s.push(cur->left);
-                continue;
-            }
-            if(cur->right && !visited.count(cur->right)) {
-                s.push(cur->right);
-                continue;
-            }
-            res.push_back(cur->val);
-            visited.insert(cur);
-            s.pop();
-        }
+        traversal(root, res);
         return res;        
+    }
+private:
+    void traversal(TreeNode* root, vector<int>& v) {
+        if (!root) return;
+        traversal(root->left, v);
+        traversal(root->right, v);
+        v.push_back(root->val);
     }
 };
 // @lc code=end
