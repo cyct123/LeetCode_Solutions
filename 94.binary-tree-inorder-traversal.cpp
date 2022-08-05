15567ea468b7b547c225e1718746691ecb3a9d47
chengziwei <chengziwei@mininglamp.com>
2022-08-05 17:15:07 +0800

    feat: optimized iterative version of 94.binary-tree-inorder-traversal.cpp

--- 94.binary-tree-inorder-traversal.cpp
+++ 94.binary-tree-inorder-traversal.cpp
@@ -16,41 +16,37 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-//#include <vector>
-//using std::vector;
-//#include <stack>
-//using std::stack;
-//#include <unordered_set>
-//using std::unordered_set;
-//
-//struct TreeNode {
-//    int val;
-//    TreeNode *left;
-//    TreeNode *right;
-//    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-//    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-//    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-//};
+#include <vector>
+using std::vector;
+#include <stack>
+using std::stack;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> res; 
         if (!root) return res;
-        unordered_set<TreeNode*> visited;
         stack<TreeNode*> s;
-        s.push(root);
-        while (!s.empty()) {
-            TreeNode* cur = s.top();
-            visited.insert(cur);
-            if (cur->left && !visited.count(cur->left)) {
-                s.push(cur->left);
-                continue;
+        TreeNode* cur = root;
+        while (cur || !s.empty()) {
+            if (cur) {
+                s.push(cur);
+                cur = cur->left;
+            } else {
+                cur = s.top();
+                res.push_back(cur->val);
+                s.pop();
+                cur = cur->right;
             }
-            res.push_back(cur->val);
-            s.pop();
-            if(cur->right && !visited.count(cur->right))
-                s.push(cur->right);
         }
         return res;
     }
