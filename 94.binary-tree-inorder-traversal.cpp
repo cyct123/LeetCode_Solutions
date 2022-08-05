f5bae867c81f6c730151c580de58f4402bd92b41
chengziwei <chengziwei@mininglamp.com>
2022-08-05 18:02:25 +0800

    feat: unified iterative version of   94.binary-tree-inorder-traversal.cpp

--- 94.binary-tree-inorder-traversal.cpp
+++ 94.binary-tree-inorder-traversal.cpp
@@ -16,36 +16,39 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-#include <vector>
-using std::vector;
-#include <stack>
-using std::stack;
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
+//#include <vector>
+//using std::vector;
+//#include <stack>
+//using std::stack;
+//
+//struct TreeNode {
+//    int val;
+//    TreeNode *left;
+//    TreeNode *right;
+//    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//};
 
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> res; 
-        if (!root) return res;
         stack<TreeNode*> s;
-        TreeNode* cur = root;
-        while (cur || !s.empty()) {
+        if (root) s.push(root);
+        while (!s.empty()) {
+            TreeNode* cur = s.top();
             if (cur) {
-                s.push(cur);
-                cur = cur->left;
-            } else {
-                cur = s.top();
-                res.push_back(cur->val);
                 s.pop();
-                cur = cur->right;
+                if (cur->right) s.push(cur->right);
+                s.push(cur);
+                s.push(nullptr);
+                if (cur->left) s.push(cur->left);
+            } else {
+                s.pop();
+                cur = s.top();
+                s.pop();
+                res.push_back(cur->val);
             }
         }
         return res;
