3c071da495085a12d11d3429ad86868f15cf29d5
chengziwei <chengziwei@mininglamp.com>
2022-08-06 11:37:47 +0800

    feat: finish iterative version of 100.same-tree.cpp

--- 100.same-tree.cpp
+++ 100.same-tree.cpp
@@ -16,12 +16,31 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//#include <vector>
+//using std::vector;
+//struct TreeNode {
+//    int val;
+//    TreeNode *left;
+//    TreeNode *right;
+//    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//};
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (!p && !q) return true;
-        if (!p || !q || p->val != q->val) return false;
-        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right); 
+        vector<TreeNode*> st = {p, q};
+        while (!st.empty()) {
+            TreeNode* left = st.back(); st.pop_back();
+            TreeNode* right = st.back(); st.pop_back();
+            if (!left && !right) continue;
+            if (!left || !right || left->val != right->val) return false;
+            st.push_back(left->left);
+            st.push_back(right->left);
+            st.push_back(left->right);
+            st.push_back(right->right);
+        }
+        return st.empty();
     }
 };
 // @lc code=end
