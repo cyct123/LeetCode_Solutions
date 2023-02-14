9e55a6f7f9d205ca655cee769150a7430de37129
chengziwei <chengziwei@mininglamp.com>
2023-02-14 14:21:23 +0800

    feat: optimize 19.remove-nth-node-from-end-of-list.py

--- 19.remove-nth-node-from-end-of-list.py
+++ 19.remove-nth-node-from-end-of-list.py
@@ -3,28 +3,84 @@
 #
 # [19] Remove Nth Node From End of List
 #
+# https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+#
+# algorithms
+# Medium (38.63%)
+# Likes:    11388
+# Dislikes: 519
+# Total Accepted:    1.5M
+# Total Submissions: 3.9M
+# Testcase Example:  '[1,2,3,4,5]\n2'
+#
+# Given the head of a linked list, remove the n^th node from the end of the
+# list and return its head.
+#
+#
+# Example 1:
+#
+#
+# Input: head = [1,2,3,4,5], n = 2
+# Output: [1,2,3,5]
+#
+#
+# Example 2:
+#
+#
+# Input: head = [1], n = 1
+# Output: []
+#
+#
+# Example 3:
+#
+#
+# Input: head = [1,2], n = 1
+# Output: [1]
+#
+#
+#
+# Constraints:
+#
+#
+# The number of nodes in the list is sz.
+# 1 <= sz <= 30
+# 0 <= Node.val <= 100
+# 1 <= n <= sz
+#
+#
+#
+# Follow up: Could you do this in one pass?
+#
+#
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 
 # @lc code=start
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
-        if head:
-            slow = ListNode(None)
-            slow.next = head
-            dummy = slow
-            fast = head
-            for _ in range(n):
-                fast = fast.next
-            while fast:
-                fast = fast.next
-                slow = slow.next
-            slow.next = slow.next.next
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        cur = head
+        length = 0
+        dummy = ListNode()
+        dummy.next = head
+        prev = dummy
+        while cur:
+            if length >= n:
+                prev = prev.next
+            length += 1
+            cur = cur.next
+        prev.next = prev.next.next
         return dummy.next
 
-# @lc code=end
 
+# @lc code=end
