01faab927dbe1d4096553aeeee433457dec78c51
chengziwei <chengziwei@mininglamp.com>
2023-02-16 20:08:38 +0800

    feat: optimize 41.first-missing-positive.py

--- 41.first-missing-positive.py
+++ 41.first-missing-positive.py
@@ -41,29 +41,22 @@
 # Your algorithm should run in O(n) time and uses constant extra space.
 #
 #
+from typing import List
+
 
 # @lc code=start
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        l = [0] * len(nums)
-        for i in nums:
-            if i <= 0 or i > len(nums):
-                continue
-            l[i - 1] += 1
-        i = 0
-        while i < len(l):
-            if l[i] == 0:
+        for i in range(len(nums)):
+            while 0 < nums[i] < len(nums) and nums[i] != nums[nums[i] - 1]:
+                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+        for i in range(len(nums)):
+            if nums[i] != i + 1:
                 return i + 1
-            i += 1
-        return i + 1
+        return len(nums) + 1
 
-class Solution1:
-    def firstMissingPositive(self, nums: List[int]) -> int:
-        num_set = set(nums)
-        counter = 1
-        while counter in num_set:
-            counter += 1
-        return counter
 
 # @lc code=end
-
+if __name__ == "__main__":
+    nums = [7, 8, 9, 11, 12]
+    print(Solution().firstMissingPositive(nums))
