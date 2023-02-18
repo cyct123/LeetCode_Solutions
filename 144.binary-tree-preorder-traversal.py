ecb1ff56c0be9f54c05a3dca1c61dd0cbeffd99f
cyct123 <cyct123@gmail.com>
2023-02-18 16:11:52 +0800

    feat: finish iterative version of 144.binary-tree-preorder-traversal.py

--- 144.binary-tree-preorder-traversal.py
+++ 144.binary-tree-preorder-traversal.py
@@ -31,6 +31,15 @@
 # Follow up: Recursive solution is trivial, could you do it iteratively?
 #
 #
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
 
 # @lc code=start
 # Definition for a binary tree node.
@@ -40,16 +49,21 @@
 #         self.left = None
 #         self.right = None
 
+
 class Solution:
     def preorderTraversal(self, root: TreeNode) -> List[int]:
         if not root:
             return []
-        vals = []
-        vals.append(root.val)
-        vals.extend(self.preorderTraversal(root.left))
-        vals.extend(self.preorderTraversal(root.right))
-        return vals
+        stack = [root]
+        res = []
+        while stack:
+            node = stack.pop()
+            res.append(node.val)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        return res
 
 
 # @lc code=end
-
