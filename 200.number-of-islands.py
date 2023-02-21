9b46ddd85baa582d55e8c4717c0c349061f6e34c
cyct123 <cyct123@gmail.com>
2023-02-21 22:10:17 +0800

    feat: finish 200.number-of-islands.py

---
+++ 200.number-of-islands.py
@@ -0,0 +1,83 @@
+#
+# @lc app=leetcode id=200 lang=python3
+#
+# [200] Number of Islands
+#
+# https://leetcode.com/problems/number-of-islands/description/
+#
+# algorithms
+# Medium (56.76%)
+# Likes:    18900
+# Dislikes: 422
+# Total Accepted:    2.1M
+# Total Submissions: 3.6M
+# Testcase Example:  '[["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]'
+#
+# Given an m x n 2D binary grid grid which represents a map of '1's (land) and
+# '0's (water), return the number of islands.
+#
+# An island is surrounded by water and is formed by connecting adjacent lands
+# horizontally or vertically. You may assume all four edges of the grid are all
+# surrounded by water.
+#
+#
+# Example 1:
+#
+#
+# Input: grid = [
+# ⁠ ["1","1","1","1","0"],
+# ⁠ ["1","1","0","1","0"],
+# ⁠ ["1","1","0","0","0"],
+# ⁠ ["0","0","0","0","0"]
+# ]
+# Output: 1
+#
+#
+# Example 2:
+#
+#
+# Input: grid = [
+# ⁠ ["1","1","0","0","0"],
+# ⁠ ["1","1","0","0","0"],
+# ⁠ ["0","0","1","0","0"],
+# ⁠ ["0","0","0","1","1"]
+# ]
+# Output: 3
+#
+#
+#
+# Constraints:
+#
+#
+# m == grid.length
+# n == grid[i].length
+# 1 <= m, n <= 300
+# grid[i][j] is '0' or '1'.
+#
+#
+#
+from typing import List
+
+
+# @lc code=start
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == "1":
+                    self.dfs(grid, i, j)
+                    count += 1
+        return count
+
+    def dfs(self, grid: List[List[str]], i: int, j: int):
+        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == "0":
+            return
+        grid[i][j] = "0"
+        self.dfs(grid, i - 1, j)
+        self.dfs(grid, i, j - 1)
+        self.dfs(grid, i + 1, j)
+        self.dfs(grid, i, j + 1)
+
+
+# @lc code=end
