e25c3ceded90381ca22c6f64e03ea7210ae713fe
cyct123 <cyct123@gmail.com>
2023-02-28 22:47:09 +0800

    feat: optimize 718.maximum-length-of-repeated-subarray.py

--- 718.maximum-length-of-repeated-subarray.py
+++ 718.maximum-length-of-repeated-subarray.py
@@ -43,18 +43,19 @@
 #
 #
 from typing import List
+
+
 # @lc code=start
 class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
         ret = 0
         dp = [[0 for _ in range(len(nums2) + 1)] for _ in range(len(nums1) + 1)]
-        for i in range(1, len(nums1)+1):
-            for j in range(1, len(nums2)+1):
-                if nums1[i-1] == nums2[j-1]:
-                    dp[i][j] = dp[i-1][j-1] + 1
-                if dp[i][j] > ret:
-                    ret = dp[i][j]
+        for i in range(1, len(nums1) + 1):
+            for j in range(1, len(nums2) + 1):
+                if nums1[i - 1] == nums2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                ret = max(ret, dp[i][j])
         return ret
 
-# @lc code=end
 
+# @lc code=end
