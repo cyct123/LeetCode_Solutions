b4d4f2842b52d6375fa92fde61da508b15cf394b
cyct123 <cyct123@gmail.com>
2023-03-02 20:46:58 +0800

    feat: finish 470.implement-rand-10-using-rand-7.py

---
+++ 470.implement-rand-10-using-rand-7.py
@@ -0,0 +1,74 @@
+#
+# @lc app=leetcode id=470 lang=python3
+#
+# [470] Implement Rand10() Using Rand7()
+#
+# https://leetcode.com/problems/implement-rand10-using-rand7/description/
+#
+# algorithms
+# Medium (46.51%)
+# Likes:    976
+# Dislikes: 326
+# Total Accepted:    70.3K
+# Total Submissions: 151.2K
+# Testcase Example:  '1'
+#
+# Given the API rand7() that generates a uniform random integer in the range
+# [1, 7], write a function rand10() that generates a uniform random integer in
+# the range [1, 10]. You can only call the API rand7(), and you shouldn't call
+# any other API. Please do not use a language's built-in random API.
+#
+# Each test case will have one internal argument n, the number of times that
+# your implemented function rand10() will be called while testing. Note that
+# this is not an argument passed to rand10().
+#
+#
+# Example 1:
+# Input: n = 1
+# Output: [2]
+# Example 2:
+# Input: n = 2
+# Output: [2,8]
+# Example 3:
+# Input: n = 3
+# Output: [3,8,10]
+#
+#
+# Constraints:
+#
+#
+# 1 <= n <= 10^5
+#
+#
+#
+# Follow up:
+#
+#
+# What is the expected value for the number of calls to rand7() function?
+# Could you minimize the number of calls to rand7()?
+#
+#
+#
+from random import randint
+
+
+def rand7():
+    return randint(1, 7)
+
+
+# @lc code=start
+# The rand7() API is already defined for you.
+# def rand7():
+# @return a random integer in the range 1 to 7
+
+
+class Solution:
+    def rand10(self):
+        """
+        :rtype: int
+        """
+        res = (rand7() - 1) * 7 + rand7() - 1
+        return self.rand10() if res >= 40 else (res % 10) + 1
+
+
+# @lc code=end
