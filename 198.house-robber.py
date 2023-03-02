4a21d53580998458bb55164f5be509847cbb0029
cyct123 <cyct123@gmail.com>
2023-03-02 20:45:35 +0800

    feat: optimize 198.house-robber.py

--- 198.house-robber.py
+++ 198.house-robber.py
@@ -43,24 +43,23 @@
 #
 #
 #
+from typing import List
+
 
 # @lc code=start
 class Solution:
     def rob(self, nums: List[int]) -> int:
         if not nums:
             return 0
+        maxTotal = 0
         dp = [0] * len(nums)
-        dp[0] = nums[0]
-        prev = 0
-        maxTotal = dp[0]
-        for i in range(1, len(nums)):
-            if prev + nums[i] > dp[i-1]:
-                dp[i] = prev + nums[i]
+        for i in range(len(nums)):
+            if i <= 1:
+                dp[i] = nums[i]
             else:
-                dp[i] = dp[i-1]
-            prev = dp[i-1]
-            maxTotal = maxTotal if maxTotal > dp[i] else dp[i]
+                dp[i] = nums[i] + max(dp[: i - 1])
+            maxTotal = max(maxTotal, dp[i])
         return maxTotal
 
-# @lc code=end
 
+# @lc code=end
